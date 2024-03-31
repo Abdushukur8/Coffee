4d7e83ce0b97ddae65462d91575bb5756eeeb1a0
Shukur <abdusukurmahmudov14@gmail.com>
2024-03-31 12:35:06 +0500

    Adaptive searchHeader

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
+// BURGER start
+
 const navbar = document.querySelector('.navbar')
 const burger = document.querySelector('.header__burger')
+
 function openBurger(){
     navbar.classList.toggle('bur_open')
 }
 burger.addEventListener('click', openBurger)
+// BURGER end
+
 
 const searchHeader = document.querySelector('.header_search')
 const searIcon = document.querySelector('.icon_search')
