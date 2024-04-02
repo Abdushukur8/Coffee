1d86793d17ade88e1c5b9fed51b9a20eea33ed21
Amirsho <amirshorakhimov0017@gmail.com>
2024-04-02 16:23:09 +0500

    Slide create but with bug

--- js/app.js
+++ js/app.js
@@ -9,9 +9,45 @@ function openBurger(){
 burger.addEventListener('click', openBurger)
 // BURGER end
 
-
+// search Header 
 const searchHeader = document.querySelector('.header_search')
 const searIcon = document.querySelector('.icon_search')
 searIcon.addEventListener('click', function(){
     searchHeader.classList.toggle('header_search_active')
 })
+// search Header 
+ 
+
+// slider main 
+
+const images = document.querySelectorAll('.slider-img')
+
+const slideControls = document.querySelectorAll('.slideBtn')
+
+let imageIndex = 0
+function show(index) {
+    images[imageIndex].classList.remove('active')
+    images[index].classList.add('active')
+    imageIndex = index
+
+}
+
+slideControls.forEach((e) => {
+    e.addEventListener('click', () =>{
+        if (event.target.classList.contains('prevBtn')){
+            let index = imageIndex - 1
+            if(index < 0){
+                index = images.length - 1
+            }
+            show(index)
+        }else if (event.target.classList.contains('nextBtn')) {
+            let index = imageIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
+            show(index);
+        }
+    })
+})
+show(imageIndex)
+// slider main 
